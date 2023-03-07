5db4519bdf0dabe878473c3b78b569802dadc7e3
0xnavigator <0xnavigator@proton.me>
2023-03-07 14:13:07 +0100

    updated tests

--- test/MasterchefGas.t.sol
+++ test/MasterchefGas.t.sol
@@ -11,23 +11,23 @@ contract MasterchefGasTest is Test {
   Masterchef mc;
 
   function setUp() public {
+    uint256 duration = 800;
     st = new BaseToken('Stake Token', 'STK');
     rw = new BaseToken('Reward Token', 'RWT');
     st.mint(address(this), 10_000 ether);
     rw.mint(address(this), 10_000 ether);
-    mc = new Masterchef(address(rw), 1 weeks);
+    mc = new Masterchef(address(rw), duration);
     st.approve(address(mc), type(uint256).max);
     rw.approve(address(mc), type(uint256).max);
     mc.add(10, address(st));
     mc.deposit(0, 100 ether);
     mc.setRewardDistributor(address(this));
     mc.updateRewards(100 ether);
-    skip(1 weeks);
+    skip(duration);
   }
 
   function testPendingFees() public {
     uint256 pending = mc.pendingReward(0, address(this));
-    console.log(pending);
-    assertTrue(pending == 100 ether - 1);
+    assertTrue(pending == 100 ether);
   }
 }

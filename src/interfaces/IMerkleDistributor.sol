9850a105d08d00972d290eea409696719dcff3ea
0xnavigator <0xnavigator@proton.me>
2023-03-15 18:53:19 +0100

    versioning

--- src/interfaces/IMerkleDistributor.sol
+++ src/interfaces/IMerkleDistributor.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity >=0.5.0;
+pragma solidity >=0.8.0;
 
 // Allows anyone to claim a token if they exist in a merkle root.
 interface IMerkleDistributor {
@@ -13,12 +13,7 @@ interface IMerkleDistributor {
   function isClaimed(uint256 index) external view returns (bool);
 
   // Claim the given amount of the token to the given address. Reverts if the inputs are invalid.
-  function claim(
-    uint256 index,
-    address account,
-    uint256 amount,
-    bytes32[] calldata merkleProof
-  ) external;
+  function claim(uint256 index, address account, uint256 amount, bytes32[] calldata merkleProof) external;
 
   // This event is triggered whenever a call to #claim succeeds.
   event Claimed(uint256 index, address account, uint256 amount);

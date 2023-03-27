11c8e8b4e35bc02b5808127e718f42813a57afef
Jiri Blaha <blaha@wedecom.cz>
2023-03-27 21:21:29 +0200

    Fixed deploy-exit10 script to accomodate latest updates

--- deploy/deploy-exit10.sh
+++ deploy/deploy-exit10.sh
@@ -24,7 +24,7 @@ LP_PER_USD="1"
 
 # Our Tokens
 NFT=$(forge create "$SRC/NFT.sol:NFT" --unlocked --constructor-args "Bond Data" "BND" 0 | extract_addr)
-STO=$(forge create "$SRC/STO.sol:STO" --unlocked --constructor-args $ZERO32 | extract_addr)
+STO=$(forge create "$SRC/STOToken.sol:STOToken" --unlocked --constructor-args $ZERO32 | extract_addr)
 BOOT=$(forge create "$SRC/BaseToken.sol:BaseToken" --unlocked --constructor-args "Exit10 Bootstrap" "BOOT" | extract_addr)
 BLP=$(forge create "$SRC/BaseToken.sol:BaseToken" --unlocked --constructor-args "Boost Liquidityp" "BLP" | extract_addr)
 EXIT=$(forge create "$SRC/BaseToken.sol:BaseToken" --unlocked --constructor-args "Exit Liquidity" "EXIT" | extract_addr)
@@ -36,8 +36,8 @@ MASTERCHEF_EXIT=$(forge create "$SRC/MasterchefExit.sol:MasterchefExit" --unlock
 
 # Exit10 Core (FeeSplitter, Exit10)
 FEE_SPLITTER=$(forge create "$SRC/FeeSplitter.sol:FeeSplitter" --unlocked --constructor-args "$MASTERCHEF0" "$MASTERCHEF1" "$SWAPPER" | extract_addr)
-EXIT10_BASE_PARAMS="($UNISWAP_V3_FACTORY,$UNISWAP_V3_NPM,$WETH,$USDC,$FEE,$LOWER_TICK,$UPPER_TICK)"
-EXIT10_DEPLOY_PARAMS="($NFT,$STO,$BOOT,$BLP,$EXIT,$MASTERCHEF_EXIT,$FEE_SPLITTER,$BOOTSTRAP_PERIOD,$ACCRUAL_PARAMETER,$LP_PER_USD)"
+EXIT10_BASE_PARAMS="($WETH,$UNISWAP_V3_FACTORY,$UNISWAP_V3_NPM,$WETH,$USDC,$FEE,$LOWER_TICK,$UPPER_TICK)"
+EXIT10_DEPLOY_PARAMS="($NFT,$STO,$BOOT,$BLP,$EXIT,$MASTERCHEF_EXIT,$FEE_SPLITTER,$BOOTSTRAP_PERIOD,$BOOTSTRAP_TARGET,$BOOTSTRAP_CAP,$LIQUIDITY_PER_USDC,$EXIT_DISCOUNT,$ACCRUAL_PARAMETER)"
 EXIT10=$(forge create "$SRC/Exit10.sol:Exit10" --unlocked --constructor-args "$EXIT10_BASE_PARAMS" "$EXIT10_DEPLOY_PARAMS" | extract_addr)
 echo "EXIT10: $EXIT10"
 
@@ -48,8 +48,6 @@ EXIT_LP="0x$(cast call "$UNISWAP_V2_FACTORY" "getPair(address,address)" "$EXIT" 
 echo "EXIT_LP: $EXIT_LP"
 
 # Post-deploy setup
-echo "sto.setExit10"
-cast send "$STO" "setExit10(address)" "$EXIT10"
 echo "nft.setExit10"
 cast send "$NFT" "setExit10(address)" "$EXIT10"
 echo "feeSplitter.setExit10"

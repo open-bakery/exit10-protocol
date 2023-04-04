2bc795546ec033de25b1eea9924a2949d75aa487
0xnavigator <0xnavigator@proton.me>
2023-04-04 12:33:53 +0200

    complete tests for redeem()

--- test/Exit10.redeem.t.sol
+++ test/Exit10.redeem.t.sol
@@ -27,4 +27,17 @@ contract Exit10_redeemTest is ABaseExit10Test {
     vm.expectRevert();
     exit10.redeem(_removeLiquidityParams(0));
   }
+
+  function test_redeem_ClaimAndDistributeFees() public {
+    (uint256 bondId, uint256 bondAmount) = _skipBootAndCreateBond();
+    skip(accrualParameter);
+    exit10.convertBond(bondId, _removeLiquidityParams(bondAmount));
+    _generateFees(token0, token1, 100000_000000);
+    uint128 liquidityToRemove = uint128(exit10.BLP().balanceOf(address(this)) / exit10.TOKEN_MULTIPLIER());
+
+    exit10.redeem(_removeLiquidityParams(liquidityToRemove));
+
+    assertGt(_balance(token0, feeSplitter), 0, 'Check balance0 feeSplitter');
+    assertGt(_balance(token1, feeSplitter), 0, 'Check balance1 feeSplitter');
+  }
 }

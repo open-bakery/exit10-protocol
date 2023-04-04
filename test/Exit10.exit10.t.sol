50e0da3fc718485eb047413f9131bcef6e5514c8
0xnavigator <0xnavigator@proton.me>
2023-04-04 14:32:03 +0200

    cleared console.

--- test/Exit10.exit10.t.sol
+++ test/Exit10.exit10.t.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
-import { console } from 'forge-std/console.sol';
 import { ABaseExit10Test } from './ABaseExit10.t.sol';
 
 contract Exit10_exit10Test is ABaseExit10Test {

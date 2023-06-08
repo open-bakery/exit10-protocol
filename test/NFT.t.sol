90fc001941a2718df56878193d9e988b2998167d
0xnavigator <0xnavigator@proton.me>
2023-06-08 09:22:03 +0200

    Deleted unused vars in test

--- test/NFT.t.sol
+++ test/NFT.t.sol
@@ -5,9 +5,7 @@ import { ABaseExit10Test } from './ABaseExit10.t.sol';
 
 contract NFT_Test is ABaseExit10Test {
   function test_NFT() public {
-    // setup: create bond for myself and alice, skip sime time to accumulate fees
-    (uint256 bondId, uint256 bondAmount) = _skipBootAndCreateBond();
-
+    _skipBootAndCreateBond();
     console.log(nft.tokenURI(1));
   }
 }
